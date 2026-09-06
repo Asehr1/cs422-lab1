d1188df13d1cff16036abcbd2f79ae62a38c929d
LXS-i <holcombe@purdue.edu>
2026-09-06 14:19:20 -0400

    Skeleton traceroute; output format

--- traceroute.sh
+++ traceroute.sh
@@ -5,10 +5,9 @@ input="listed_iperf3_servers.csv"
 echo "hops,lat" > hops1.csv
 
 tail -n 1 $input | while IFS="," read -r ip other; do
-  tr=$(traceroute -n -q 1 $ip)
-  tail -n +2 $tr | while IFS=" " read -r hops addr lat ms; do
-    if [[ "$addr" != "*" ]]; then
-      echo $hops","$lat >> hops1.csv
-    fi
-  done
+  tr=$(traceroute -n -q 1 $ip | tail -n +2)
+  IFS=" " read -r hops addr lat ms <<< "$tr"
+  if [[ "$addr" != "*" ]]; then
+    echo $hops","$lat >> hops1.csv
+  fi
 done
