aa3457d53b9d668199acf0ecb3be11526d49dd0d
Arnav0000 <arnav080106@gmail.com>
2026-09-09 10:56:50 -0400

    made fullscript run other files

--- fullscript.sh
+++ fullscript.sh
@@ -1,27 +1,17 @@
 #!/bin/bash
 
-input="listed_iperf3_servers.csv"
+echo "Creating Python virtual environment"
+python3 -m venv .venv
 
-echo "ip,min,avg,max,lat,lon" > rtt.csv
+echo "Activating Python virtual environment"
+source .venv/bin/activate
 
-tail +2 $input | while IFS="," read -r ip other; do
-	location=$(curl "http://ip-api.com/csv/$ip?fields=status,lat,lon" 2>/dev/null)
-	IFS="," read -r status lat lon <<< "$location"
-	if [[ "$status" == "success" ]]; then
-		ping $ip -c 10 -i 0.002 | grep 'min/avg/max' | awk -F '= ' '{print $2}' | awk -v ip=$ip -v lat=$lat -v lon=$lon -F '/' '{print ip "," $1 "," $2 "," $3 "," lat "," lon}' >> rtt.csv
-	fi
-done
+echo "Installing required packages"
+pip install -r requirements.txt
 
+echo "Running part 1"
+python3 part_1.py
 
-shuf -n 5 "listed_iperf3_servers.csv" | while IFS="," read -r ip other; do
-  echo "hops,lat" > "$ip.csv"
-  mapfile lines < <(traceroute -n -q 1 $ip | tail -n +2)
-  for line in "${lines[@]}"; do
-    IFS=" " read -r hops addr lat ms <<< $line
-    if [[ "$addr" != "*" ]]; then
-      echo "$hops,$lat" >> "$ip.csv"
-    else
-      echo "$hops,NONE" >> "$ip.csv"
-    fi
-  done
-done
+echo "Running part 2"
+./traceroute.sh
+python3 p2_plots.py
